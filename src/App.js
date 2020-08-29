13bbbe128bdc013ba3e6ec177afb30496460e25b
Drsultanali <sultan_ali338@yahoo.com>
2020-08-28 23:11:37 -0700

    React project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Salam From Sultan Ali
+    </div>
+  );
+}
+
+export default App;
